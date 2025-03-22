2aa5c0787d70603355e765d04037253f418bb3c7
radu.nasoi <radu_nasoi@yahoo.com>
2025-03-22 14:21:55 +0200

    Add user data on startup

--- src/main/java/com/example/StoreManagement/StoreManagementApplication.java
+++ src/main/java/com/example/StoreManagement/StoreManagementApplication.java
@@ -1,13 +1,28 @@
 package com.example.StoreManagement;
 
+import com.example.StoreManagement.model.User;
+import com.example.StoreManagement.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StoreManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StoreManagementApplication.class, args);
+	}
+
+	//Add some mock users on startup
+	@Bean
+	public CommandLineRunner seedUsers(UserRepository userRepository) {
+		return args -> {
+			if (userRepository.count() == 0) {
+				userRepository.save(new User(null, "admin", "admin123", "ADMIN"));
+				userRepository.save(new User(null, "user", "user123", "USER"));
+			}
+		};
 	}
 
 }
