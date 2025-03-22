dcbf14c49f5d1357fdda9675640c1a56d6456797
radu.nasoi <radu_nasoi@yahoo.com>
2025-03-22 21:04:27 +0200

    Create StoreManagementConstants class

---
+++ src/main/java/com/example/StoreManagement/util/StoreManagementConstants.java
@@ -0,0 +1,14 @@
+package com.example.StoreManagement.util;
+
+public class StoreManagementConstants {
+    public static final String PRODUCT_NOT_FOUND = "Attempted to change price for non-existing product ID {}";
+    public static final String PRODUCT_NOT_FOUND_ERROR_MESSAGE = "Product not found";
+    public static final String USER_NOT_FOUND_ERROR_MESSAGE = "User not found";
+    public static final String ROLE = "role";
+    public static final String USERNAME = "username";
+    public static final String ADDING_PRODUCT = "Adding product: {}";
+    public static final String SAVED_PRODUCT = "Saved product: {}";
+    public static final String GET_PRODUCT_BY_ID = "Get product by id: {}";
+    public static final String CHANGING_PRICE = "Changing price for product ID {} to {}";
+    public static final String CHANGING_QUANTITY = "Changing quantity for product ID {} to {}";
+}
